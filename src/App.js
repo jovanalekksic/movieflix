f5ece36fb64b4b0778545c90ad4b7c49df8ec7bd
Jovana Aleksic <jaleksic23@gmail.com>
2023-03-03 00:31:06 +0100

    dodata pozadina i stilizovana pocetna strana

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
+import Register from './components/Register';
+import Login from './components/Login'
 import HomeBody from './components/HomeBody';
+
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
       <NavBar/>
-      <HomeBody/>
+      <Routes>
+        <Route path='/' element={<HomeBody/>}  />
+        <Route path='/register' element={<Register/>}  />
+        <Route path='/login' element={<Login/>}  />
+      </Routes>
+      
       <Footer/>
     </BrowserRouter>
   );
--- src/components/HomeBody.jsx
+++ src/components/HomeBody.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import tv from "../images/tv.png";
+import phone from "../images/phone.png";
+import devices from "../images/devices.png";
 
 const HomeBody = () => {
   return (
-    <div className="homeBody">
-      <div className="title">
-        <h1>Unlimited movies, TV shows, and more.</h1>
-        <p style={{ fontSize: 32 }}>Plans now start at EUR4.99/month</p>
+    <div className="slika">
+      <div className="homeBody">
+        <div className="title">
+          <h1>Unlimited movies, TV shows, and more.</h1>
+          <p style={{ fontSize: 32 }}>Plans now start at EUR4.99/month</p>
+          <button type="button" className="btn btn-danger" id="buttonJoin">
+            JOIN
+          </button>
+        </div>
       </div>
       <div className="tv">
         <div className="tvtext">
@@ -16,7 +24,32 @@ const HomeBody = () => {
           </p>
         </div>
         <div className="imagetv">
-          <img src="images/tv.png" />
+          <img src={tv} />
+        </div>
+      </div>
+
+      <div className="phone">
+        <div className="imagephone">
+          <img src={phone} />
+        </div>
+        <div className="phoneText">
+          <h1>Download your shows to watch offline.</h1>
+          <p style={{ fontSize: 32 }}>
+            Save your favorites easily and always have something to watch.
+          </p>
+        </div>
+      </div>
+
+      <div className="tv">
+        <div className="tvtext">
+          <h1>Watch everywhere.</h1>
+          <p style={{ fontSize: 32 }}>
+            Stream unlimited movies and TV shows on your phone, tablet, laptop,
+            and TV without paying more.
+          </p>
+        </div>
+        <div className="imagetv">
+          <img src={devices} />
         </div>
       </div>
     </div>
