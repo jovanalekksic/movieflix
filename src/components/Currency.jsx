b64cfdb9e3fe48fee8a417e86e13947ffe70e4df
Jovana Aleksic <jaleksic23@gmail.com>
2023-05-17 23:18:16 +0200

    dodate komponente i stajling za valute

---
+++ src/components/Currency.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Currency = () => {
+  return (
+    <div className="group">
+      <input type="text" value={1} />
+      <select name="" id=""></select>
+    </div>
+  );
+};
+
+export default Currency;
