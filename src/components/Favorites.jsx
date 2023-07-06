96346c79728efdfd1187330eacc60e70cde4179a
Jovana Aleksic <jaleksic23@gmail.com>
2023-07-06 20:31:16 +0200

    izmenjen tekst

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -3,28 +3,6 @@ import OneFavorite from "./OneFavorite";
 import { Outlet } from "react-router-dom";
 
 const Favorites = ({ token }) => {
-  const decodeToken = (token) => {
-    if (!token) {
-      return null;
-    }
-    const base64Url = token.split(".")[1];
-    const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
-    const jsonPayload = decodeURIComponent(
-      atob(base64)
-        .split("")
-        .map((c) => {
-          return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
-        })
-        .join("")
-    );
-
-    const { sub: userId } = JSON.parse(jsonPayload);
-    return userId;
-  };
-
-  const user_id = token ? decodeToken(token) : null;
-  console.log(user_id);
-
   return (
     <div>
       <div className="allMovies">
