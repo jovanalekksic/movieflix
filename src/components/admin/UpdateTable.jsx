653333b6953da1e9c5075e788f5da8bffcabdf6f
Jovana Aleksic <jaleksic23@gmail.com>
2023-07-07 01:58:27 +0200

    sredjen izgled forme za izmenu

--- src/components/admin/UpdateTable.jsx
+++ src/components/admin/UpdateTable.jsx
@@ -78,8 +78,8 @@ function Update() {
         <section className="vh-100 gradient-custom">
           <div className="container py-5 h-100">
             <div className="row d-flex justify-content-center align-items-center h-100">
-              <div className="col-12 col-md-8 col-lg-6 col-xl-5">
-                <div className="card  text-white" id="forms">
+              <div className="col-12 col-md-8 col-lg-6 col-xl-5 w-75">
+                <div className="card  text-white " id="formsUpdate">
                   <div className="card-body p-5 text-center">
                     <div className="mb-md-5 mt-md-4 pb-5">
                       <h2 className="fw-bold mb-2 text-uppercase">
